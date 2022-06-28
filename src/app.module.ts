51546f83fd2f969b374cf9a888d778335be6336e
Ashiru Adedayo <Ashiru Adedayo>
2022-06-28 22:03:44 +0100

    Added Database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
+import { typeOrmConfigAsync } from './config/typeorm.config';
 
 @Module({
-  imports: [UserModule, UsersModule, ReportsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    UsersModule,
+    ReportsModule,
+    TypeOrmModule.forRootAsync(typeOrmConfigAsync),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    })
+  ],
 })
 export class AppModule {}
