6b3e12152eb5cdc4e33f13b53ee07e8a459855db
Ashiru Adedayo <Ashiru Adedayo>
2022-07-04 13:42:01 +0100

    user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { typeOrmConfigAsync } from './config/typeorm.config';
-import { User } from './users/user.entity';
+import { UserModule } from './user/user.module';
+
+
 
 @Module({
   imports: [
-    UsersModule,
     ReportsModule,
     TypeOrmModule.forRootAsync(typeOrmConfigAsync),
-    TypeOrmModule.forFeature([User]),
     ConfigModule.forRoot({
       isGlobal: true,
-    })
+    }),
+    UserModule,
+    
   ],
 })
 export class AppModule {}
